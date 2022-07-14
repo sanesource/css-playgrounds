268abde2a4509ceea0de0dbd2a3025f668ea3592
Akul Srivastava <akulsr0@gmail.com>
2022-07-14 15:30:06 +0530

    fix: build

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,8 +6,14 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/sass.js/0.11.1/sass.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/sass.js/0.11.1/sass.worker.min.js"></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/sass.js/0.11.1/sass.min.js"
+          async
+        ></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/sass.js/0.11.1/sass.worker.min.js"
+          async
+        ></script>
         <NextScript />
       </body>
     </Html>
